05943b015f071437a15d2a70d3a7cc41bc5273f2
jeffrey moon <novmoon73@gmail.com>
2015-02-06 09:54:22 +0900

    first commit

---
+++ purity00.js
@@ -0,0 +1,44 @@
+var _ = require("underscore");
+
+var rand = partial1(_.random, 1);
+console.log("rand(10) => ", rand(10));
+console.log("repeatedly(10, partial1(rand, 10)) => ",repeatedly(10, partial1(rand, 10)));
+console.log("_.take(repeatedly(100, partial1(rand. 10)), 5) => ", _.take(repeatedly(100, partial1(rand, 10)), 5));
+console.log(randString(0));
+console.log(randString(1));
+console.log(randString(10));
+
+
+function randString(len) {
+	var ascii = repeatedly(len, partial1(rand, 26));
+	return _.map(ascii, function(n) {
+		return n.toString(36);
+	}).join("");
+}
+
+function partial1(fun, arg1) {
+	return function(/* args */) {
+		var args = construct(arg1, arguments);
+		return fun.apply(fun, args);
+	}
+}
+
+function construct(head, tail) {
+	return cat([head], _.toArray(tail));
+}
+
+function cat() {
+	var head = _.first(arguments);
+	if (existy(head))
+		return head.concat.apply(head, _.rest(arguments));
+	else
+		return [];
+}
+
+function existy(x) {
+	return x != null;
+}
+
+function repeatedly(times, fun) {
+	return _.map(_.range(times), fun);
+}
---
+++ purity02.js
@@ -0,0 +1,48 @@
+var _ = require("underscore");
+var rand = partial1(_.random, 1);
+var composedRandomString = partial1(generateString, generateRandomCharacter);
+
+console.log("generateString(generateRandomCharacter, 20) => ", generateString(generateRandomCharacter, 20));
+console.log("composedRandomString(10) => ", composedRandomString(10));
+
+function generateRandomCharacter() {
+	return rand(26).toString(36);
+}
+
+function generateString(charGen, len) {
+	return repeatedly(len, charGen).join('');
+}
+
+function randString(len) {
+	var ascii = repeatedly(len, partial1(rand, 26));
+	return _.map(ascii, function(n) {
+		return n.toString(36);
+	}).join("");
+}
+
+function partial1(fun, arg1) {
+	return function(/* args */) {
+		var args = construct(arg1, arguments);
+		return fun.apply(fun, args);
+	}
+}
+
+function construct(head, tail) {
+	return cat([head], _.toArray(tail));
+}
+
+function cat() {
+	var head = _.first(arguments);
+	if (existy(head))
+		return head.concat.apply(head, _.rest(arguments));
+	else
+		return [];
+}
+
+function existy(x) {
+	return x != null;
+}
+
+function repeatedly(times, fun) {
+	return _.map(_.range(times), fun);
+}
